d7a3f504630e011d205d9c46eab989ed8fce6049
Hawas Muhaba <hawasmuhaba4socials@gmail.com>
2025-08-10 22:49:47 +0300

    small fixes

--- database.js
+++ database.js
@@ -35,7 +35,7 @@ async function saveUserLocation(chat_id, first_name, latitude,longitude) {
     
     //update their detail instead of trying new row insertion
     await db.run(
-        `INSERT INTO user (chat_id, first_name, latitude, longitude)
+        `INSERT INTO users (chat_id, first_name, latitude, longitude)
          VALUES (?, ?, ?, ?)
          ON CONFLICT(chat_id) DO UPDATE SET
             first_name = excluded.first_name,
@@ -52,7 +52,7 @@ async function saveUserLocation(chat_id, first_name, latitude,longitude) {
 async function getAllActiveUsers(){
     const db = await dbPromise;
     //selec users where active,1
-    const users = await db.all('SELECT * FROM user Where is_active = 1');
+    const users = await db.all('SELECT * FROM users Where is_active = 1');
     return users;
 }
 
